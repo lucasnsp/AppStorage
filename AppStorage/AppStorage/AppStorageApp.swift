07dad7c2e0866e240643ff66227f09ee7b4502c2
Lucas Neves <122572631+lucasnsp@users.noreply.github.com>
2023-11-21 11:34:35 -0300

    Add files via upload

---
+++ AppStorage/AppStorage/AppStorageApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppStorageApp.swift
+//  AppStorage
+//
+//  Created by Lucas Neves dos santos pompeu on 21/11/23.
+//
+
+import SwiftUI
+
+@main
+struct AppStorageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
