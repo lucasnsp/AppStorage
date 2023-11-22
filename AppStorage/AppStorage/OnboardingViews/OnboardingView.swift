1300bd3dc7fcffc9cca3925a018b3aea6f6eae98
Lucas Neves <lucas.neves12@icloud.com>
2023-11-22 10:54:20 -0300

    Creating Welcome Section and add nameSection.

--- AppStorage/AppStorage/OnboardingViews/OnboardingView.swift
+++ AppStorage/AppStorage/OnboardingViews/OnboardingView.swift
@@ -16,7 +16,8 @@ struct OnboardingView: View {
      2 - Add Age
      3 - Add gender
     */
-    @State var onboardingState: Int = 0
+    @State var onboardingState: Int = 1
+    @State var name: String = ""
     
     var body: some View {
         ZStack {
@@ -25,6 +26,8 @@ struct OnboardingView: View {
                 switch onboardingState {
                 case 0:
                     welcomeSection
+                case 1:
+                    addNameSection
                 default:
                     RoundedRectangle(cornerRadius: 25)
                         .foregroundStyle(.green)
@@ -65,7 +68,49 @@ extension OnboardingView {
     
     private var welcomeSection: some View {
         VStack(spacing: 40) {
-            Text("Hi")
+            Spacer()
+            Image(systemName: "heart.text.square.fill")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 200, height: 200)
+                .foregroundStyle(.white)
+            Text("Find your match.")
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+                .foregroundStyle(.white)
+                .overlay(
+                    Capsule(style: .continuous)
+                        .frame(height: 3)
+                        .offset(y: 5)
+                        .foregroundStyle(.white)
+                    ,alignment: .bottom
+                )
+            Text("This is the #1 app for finding your match online! In this tutorial we are practicing using AppStorage and other SwiftUI techniques.")
+                .fontWeight(.medium)
+                .foregroundStyle(.white)
+            Spacer()
+            Spacer()
         }
+        .multilineTextAlignment(.center)
+        .padding(30)
+    }
+    
+    private var addNameSection: some View {
+        VStack(spacing: 40) {
+            Spacer()
+            Text("What's your name?")
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+                .foregroundStyle(.white)
+            TextField("Your name here...", text: $name)
+                .font(.headline)
+                .frame(height: 55)
+                .padding(.horizontal)
+                .background(Color.white)
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+            Spacer()
+            Spacer()
+        }
+        .padding(30)
     }
 }
